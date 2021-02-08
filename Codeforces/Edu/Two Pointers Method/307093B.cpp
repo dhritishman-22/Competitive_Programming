31090bff2a71d47eaa595bd5548e125943d6e273
Dhritishman Kotaki <59554995+dhritishman-22@users.noreply.github.com>
2021-02-09 01:25:37 +0530

    Update 307093B.cpp

--- Codeforces/Edu/Two Pointers Method/307093B.cpp
+++ Codeforces/Edu/Two Pointers Method/307093B.cpp
@@ -1,1 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pi 3.1415926535
+#define int long long
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ll long long
+#define setbits(x) __builtin_popcountll(x)
+#define pb push_back
+#define sz(x) (int)x.size()
+#define f(i,a,n) for(int i = a ; i < n ; i++)
+#define F first
+#define S second
+#define all(c) (c).begin(),(c).end()
+#define get(a,n) f(i,0,n) cin >> a[i];
+#define show(a,n) f(i,0,n) cout << a[i] << " "; cout << endl;
+#define nitro ios:: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define trace(x) cerr << '>' << #x << ':' << (x) << "\n"
+#define trace2(x,y) cerr<< '>' << #x << ':' << (x) << " | " << #y << ':' << (y) << "\n"
+#define trace3(a,b,c) cerr<<#a<<"="<<(a)<<", "<<#b<<"="<<(b)<<", "<<#c<<"="<<(c)<<"\n"
+#define trace4(a,b,c,d) cerr<<#a<<"="<<(a)<<", "<<#b<<"="<<(b)<<", "<<#c<<"="<<(c)<<", "<<#d<<"="<<(d)<<"\n"
+
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef map<int,int> mii;
+typedef map<ll,ll> mll;
+
+const int inf = 1e9;
+const int inf64 = 1e18;
+const int mod = inf + 7;
+const int N = 2e5 + 50;
+
+void solve()
+{
+    int n, s;
+    cin >> n >> s;
+    vector<int> a(n);
+    for(auto &x : a)
+        cin >> x;
+
+    int l = 0, r = 0, ans = inf64, sum = 0;
+    while(r < n)
+    {
+        sum += a[r];
+        while(sum - a[l] >= s)
+        {
+            sum -= a[l];
+            l++;
+        }
+        if(sum >= s)
+            ans = min(ans, r - l + 1);
+        r++;
+    }
+    if(ans == inf64)
+        ans = -1;
+    cout << ans;
+}
+
+int32_t main()
+{
+    nitro;
+    int tc = 1;
+//    cin >> tc;
+    while(tc--) solve();
+}
 
