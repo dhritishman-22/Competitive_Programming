1fbce031990a7782b319add27fed17f9bdd2b1b1
Dhritishman Kotaki <59554995+dhritishman-22@users.noreply.github.com>
2021-03-08 21:21:38 +0530

    Create A.cpp

---
+++ Codeforces/Div 2 Contests/CF Round 705/A.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pi 3.1415926535
+#define int long long
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ll long long
+#define setbits(x) __builtin_popcountll(x)
+#define pb push_back
+#define sz(x) (int)x.size()
+#define f(i,a,n) for(int i = a ; i < n ; i++)
+#define F first
+#define S second
+#define all(c) (c).begin(),(c).end()
+#define get(a,n) f(i,0,n) cin >> a[i];
+#define show(a,n) f(i,0,n) cout << a[i] << " "; cout << endl;
+#define nitro ios:: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define trace(x) cerr << '>' << #x << ':' << (x) << "\n"
+#define trace2(x,y) cerr<< '>' << #x << ':' << (x) << " | " << #y << ':' << (y) << "\n"
+#define trace3(a,b,c) cerr<<#a<<"="<<(a)<<", "<<#b<<"="<<(b)<<", "<<#c<<"="<<(c)<<"\n"
+#define trace4(a,b,c,d) cerr<<#a<<"="<<(a)<<", "<<#b<<"="<<(b)<<", "<<#c<<"="<<(c)<<", "<<#d<<"="<<(d)<<"\n"
+
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef map<int,int> mii;
+typedef map<ll,ll> mll;
+
+const int inf = 2e9;
+const int inf64 = 2e18;
+const int mod = 1000000007;
+const int N = 1e6 + 100;
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    set<int> s;
+    vector<int> ans;
+    for(int i = n; i >= 1; i--)
+    {
+        if(i == k)
+            continue;
+        if(s.find(k - i) == s.end())
+            ans.push_back(i);
+        s.insert(i);
+    }
+    cout << (int)ans.size() << "\n";
+    for(int x : ans)
+        cout << x << ' ';
+    cout << endl;
+}
+
+int32_t main()
+{
+    nitro;
+    int tc = 1;
+    cin >> tc;
+    while(tc--) solve();
+}
